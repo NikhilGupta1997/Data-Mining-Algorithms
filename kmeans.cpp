03a32cd4cb72a035f6249b87026723e5c07b1788
NikhilGupta1997 <nikhilgupta1997@gmail.com>
2018-09-17 18:16:15 +0530

    KMeans Implemented (Done)

--- kmeans.cpp
+++ kmeans.cpp
@@ -4,6 +4,8 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <time.h> 
+#include <math.h>
+#include <fstream>
 #include "helper.cpp"
 
 using namespace std;
@@ -13,15 +15,81 @@ int myrandom (int i) { return std::rand()%i;}
 class KMeans {
   private: 
   	vector<vector <coord> > clusters;
+  	vector<vector<int> > closest;
+  	double error, threshold = 0.00001;
 
   public:
   	void add_cluster(vector<coord> point) {
+  		assert(!point.empty());
   		clusters.push_back(point);
   	}
 
   	void update_cluster(int index, vector<coord> point) {
   		assert(index < clusters.size());
+  		assert(!point.empty());
   		clusters[index] = point;
+  	}
+
+  	double eu_distance(vector<coord> point, vector<coord> cluster) {
+  		double val = 0.0;
+  		for (int i = 0; i < point.size(); i++) {
+  			val += pow(point[i] - cluster[i], 2);
+  		}
+  		return val;
+  	}
+
+  	void map_points(PointData data) {
+  		for(int i = 0; i < clusters.size(); i++) {
+  			closest[i].clear();
+  		}
+
+  		for(int i = 0; i < data.size(); i++) {
+  			vector<coord> point = data[i];
+  			double distance = -1.0, dist;
+  			int index = 0;
+  			for(int j = 0; j < clusters.size(); j++) {
+  				dist = eu_distance(point, clusters[j]);
+  				if(distance == -1.0 || dist < distance) {
+  					distance = dist;
+  					index = j;
+  				}
+  			}
+  			closest[index].push_back(i);
+  		}
+  	}
+
+  	void vector_add(vector <coord>& a, vector <coord> b) {
+  		for(int i = 0; i < a.size(); i++) {
+  			a[i] = a[i] + b[i];
+  		}
+  	}
+
+  	void vector_divide(vector <coord>& a, int b) {
+  		for(int i = 0; i < a.size(); i++) {
+  			a[i] = a[i] / double(b);
+  		}
+  	}
+
+  	double calculate_clusters(PointData data) {
+  		double total_error = 0.0;
+  		for(int i = 0; i < clusters.size(); i++) {
+  			vector<coord> v;
+  			for(auto point : closest[i]) {
+  				if(v.empty()) {
+  					v = data[point];
+  				}
+  				else {
+  					vector_add(v, data[point]);
+  				}
+  			}
+  			if (v.empty()) {
+  				continue;
+  			}
+  			vector_divide(v, closest[i].size());
+  			total_error += eu_distance(v, clusters[i]);
+  			update_cluster(i, v);
+  		}
+  		return total_error;
   	}
 
   	KMeans(PointData data, int k) {
@@ -32,6 +100,14 @@ class KMeans {
 		random_shuffle(index.begin(), index.end(), myrandom);
 		for(int i = 0; i < k; i++) {
 	  		add_cluster(data[index[i]]);
+	  		vector<int> row;
+		    closest.push_back(row);
+		}
+		map_points(data);
+		error = calculate_clusters(data);
+		while(threshold < error) {
+			map_points(data);
+			error = calculate_clusters(data);
 		}
   	}
 
@@ -42,6 +118,19 @@ class KMeans {
 	    	}
 	    	cout << endl;
 	    }
+  	}
+
+  	void output() {
+  		ofstream myfile ("kmeans.txt");
+  		assert(myfile.is_open());
+  		for (int i = 0; i < clusters.size(); i++) {
+  			myfile << "#" << i << "\n";
+  			for (auto ele : closest[i]) {
+  				myfile << ele << "\n";
+  			}
+  			myfile << "\n";
+  		}
+		myfile.close();
   	}
 
 };
@@ -56,5 +145,6 @@ int main(int argc, char **argv){
     
     KMeans clusters(data, k);
     clusters.print();
+    clusters.output();
 	return 0;
 }
