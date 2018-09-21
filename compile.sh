31a9b95b2106808a661bcf3b40a90a1684b747e2
NikhilGupta1997 <nikhilgupta1997@gmail.com>
2018-09-21 14:52:51 +0530

    Submission Files Added

--- compile.sh
+++ compile.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#git clone https://github.com/NikhilGupta1997/Data-Clustering.git
 g++ -o kmeans -std=c++11 -O3 kmeans.cpp
 g++ -o dbscan -std=c++11 -O3 dbscan.cpp
 g++ -o optics -std=c++11 -O3 optics.cpp
